205208a95bc3b240d86170598f42ea8597583f76
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-05-16 22:48:04 +0530

    Create Addition Under Modulo .cpp

---
+++ Addition Under Modulo .cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        long long sumUnderModulo(long long a,long long b)
+    {
+        int m=1000000007;
+        a=a%m;
+        b=b%m;
+        return (a+b)%m;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    long long a;
+	    long long b;
+	    cin>>a>>b;
+	    Solution ob;
+	    cout<<ob.sumUnderModulo(a,b)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
