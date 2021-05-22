a5f1c0ed08ef6ca24b8265cfac23fe2c446d741f
Abhishek Kumar <60790645+Abhi23raj472@users.noreply.github.com>
2021-05-23 00:15:26 +0530

    Create Binary Search.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,52 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    int binarysearch(int arr[], int n, int key){
+        int i;
+        int low=0,high=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]==key){
+                return mid;
+            }
+            if(arr[mid]>key){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i=0;i<N;i++)
+            cin>>arr[i];
+        int key;
+        cin>>key;
+        Solution ob;
+        int found = ob.binarysearch(arr,N,key);
+        cout<<found<<endl;
+    }
+}
+
+  // } Driver Code Ends
